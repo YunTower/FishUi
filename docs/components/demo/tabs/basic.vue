d3ab4fd441bab756365288a159cda11f6cc67571
fish <xyfishcn@qq.com>
2025-05-30 09:05:11 +0800

    refactor(tabs): 优化标签页组件样式和功能
    - 在基本示例中添加 tab-key属性，以提高标签页的可控性
    - 优化标签页样式，包括悬浮状态和卡片模式下的样式
    - 修复部分浏览器下可能出现的滚动条问题

--- docs/components/demo/tabs/basic.vue
+++ docs/components/demo/tabs/basic.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <f-tabs v-model:activeTab="activeTab">
-      <f-tab-pane title="标签1">标签1内容</f-tab-pane>
-      <f-tab-pane title="标签2">标签2内容</f-tab-pane>
-      <f-tab-pane title="标签3">标签3内容</f-tab-pane>
+      <f-tab-pane tab-key="1" title="标签1">标签1内容</f-tab-pane>
+      <f-tab-pane tab-key="2" title="标签2">标签2内容</f-tab-pane>
+      <f-tab-pane tab-key="3" title="标签3">标签3内容</f-tab-pane>
     </f-tabs>
     <div style="margin-top: 16px;">
       当前选中的标签：{{ activeTab }}
